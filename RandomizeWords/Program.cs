5bdb0e561758c39ca56fc7feb5b6493cb669151f
Rozhidar <148547922+Rozhidar@users.noreply.github.com>
2025-08-02 23:00:29 +0200

    Change first task

--- RandomizeWords/Program.cs
+++ RandomizeWords/Program.cs
@@ -4,13 +4,21 @@ Random random = new Random();
 
 for (int i = 0; i < words.Length; i++)
 {
-    int randomIndex = random.Next(i, words.Length);
-    string temp = words[i];
-    words[i] = words[randomIndex];
-    words[randomIndex] = temp;
+    int randomIndex = random.Next(0, words.Length);
+    Swap(i, randomIndex, words);
+    //string temp = words[i];
+    //words[i] = words[randomIndex];
+    //words[randomIndex] = temp;
 }
 
 foreach (string word in words)
 {
     Console.WriteLine(word);
+}
+
+static void Swap(int firstIndex, int secondIndex, string[] array)
+{
+    string temp = array[firstIndex];
+    array[firstIndex] = array[secondIndex];
+    array[secondIndex] = temp;
 }
