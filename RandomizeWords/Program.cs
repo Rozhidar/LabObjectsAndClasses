253d937be9575ec5092e2742c54b1d414d42be9c
Rozhidar <148547922+Rozhidar@users.noreply.github.com>
2025-08-02 22:40:34 +0200

    Add project files.

---
+++ RandomizeWords/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] words = Console.ReadLine().Split();
+
+Random random = new Random();
+
+for (int i = 0; i < words.Length; i++)
+{
+    int randomIndex = random.Next(i, words.Length);
+    string temp = words[i];
+    words[i] = words[randomIndex];
+    words[randomIndex] = temp;
+}
+
+foreach (string word in words)
+{
+    Console.WriteLine(word);
+}
